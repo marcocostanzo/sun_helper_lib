fe7bc3977b68b8f5f3ddcb4d49d694d0324b1b1e
Marco Costanzo <marco.costanzo@unicampania.it>
2018-09-24 15:12:11 +0200

    Added unicampania_helper_lib package

---
+++ include/Helper.h
@@ -0,0 +1,78 @@
+/*
+    Helper Lib
+
+    Copyright 2017-2018 Università della Campania Luigi Vanvitelli
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef MYLIB_HELPER_H
+#define MYLIB_HELPER_H
+
+#include <TooN/TooN.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+/* ======= COLORS ========= */
+#define CRESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+/*===============================*/
+
+//TooN::Vector<> readFileV(const char* path,unsigned  int dim);
+TooN::Vector<> readFileV(const std::string& path,unsigned  int dim);// { return readFileV(path.c_str() , dim); }
+//TooN::Matrix<> readFileM(const char* path,unsigned  int n_r,unsigned  int n_c);
+TooN::Matrix<> readFileM(const std::string& path,unsigned  int n_r,unsigned  int n_c);//{ return readFileM(path.c_str(), n_r, n_c); }
+
+#ifndef QUATERNION_H
+TooN::Matrix<3,3> skew( TooN::Vector<3> v );
+#endif
+
+/* == ASK FOR CHAR == */
+/* USAGE
+	char ans = askForChar( "Continue? [y = YES / n = nextIter / e = exit ]: " );
+		switch( ans ){
+			case 'n' :
+			case 'N' :
+				continue;
+			case 'y' :
+			case 'Y' :
+			case 's' :
+			case 'S' :
+				break;
+			default:
+				exit(1);			
+		}
+		ans = 0;
+	*/
+char askForChar( const char* str);
+
+#endif
---
+++ src/Helper/Helper.cpp
@@ -0,0 +1,102 @@
+/*
+    Helper Lib
+
+    Copyright 2017-2018 Università della Campania Luigi Vanvitelli
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Helper.h>
+
+using namespace TooN;
+using namespace std;
+
+Vector<> readFileV(const string& path, unsigned int dim){
+	FILE *f;
+	f = fopen(path.c_str(), "r");
+	if (f == NULL){
+		printf(BOLDRED "Error opening file..." CRESET);
+		printf(BOLDBLUE " %s\n" CRESET,path.c_str());
+		exit(1);
+	}
+
+	Vector<> out = Zeros(dim);
+	double tmp;
+	for (int i = 0; i < dim; i++) {
+		fscanf(f, "%lf,", &tmp);
+		out[i] = tmp;
+	}
+	fclose(f);
+
+	return out;
+}
+
+
+
+Matrix<> readFileM(const string& path, unsigned int n_r, unsigned int n_c){
+	FILE *f;
+	f = fopen(path.c_str(), "r");
+	if (f == NULL){
+		printf(BOLDRED "Error opening file..." CRESET);
+		printf(BOLDBLUE " %s\n" CRESET,path.c_str());
+		exit(1);
+	}
+
+	Matrix<> out = Zeros(n_r,n_c);
+	double tmp;
+	for (int i = 0; i < n_r; i++) {
+		for (int j = 0; j < n_c; j++) {
+			fscanf(f, "%lf", &tmp);
+			out[i][j] = tmp;
+		}
+	}
+	fclose(f);
+
+	return out;
+}
+
+Matrix<3,3> skew( Vector<3> v ){
+	return Data( 0.0, -v[2] , v[1],
+				v[2],  0.0  , -v[0],
+				-v[1], v[0] , 0.0);
+}
+
+
+
+
+char askForChar( const char* str){
+	/* USAGE
+	char ans = askForChar( "Continue? [y = YES / n = nextIter / e = exit ]: " );
+		switch( ans ){
+			case 'n' :
+			case 'N' :
+				continue;
+			case 'y' :
+			case 'Y' :
+			case 's' :
+			case 'S' :
+				break;
+			default:
+				exit(1);			
+		}
+		ans = 0;
+	*/
+
+	char ans;
+	cout << str;
+	cin >> ans;
+	cout << endl;
+
+	return ans;
+}
